b790d9940988c01d93f2055b68b7d2c4442f32b4
mskalisz <mskalisz3@gmail.com>
2022-05-22 19:35:58 -0400

    Add comments for what similar routes do

--- todo.rb
+++ todo.rb
@@ -15,16 +15,19 @@ get "/" do
   redirect "/lists"
 end
 
+# View all lists
 get "/lists" do
   @lists = session[:lists]
 
   erb :lists, layout: :layout
 end
 
+# Render new list form
 get "/lists/new" do
   erb :new_list, layout: :layout
 end
 
+# Create a new list
 post "/lists" do
   session[:lists] << {name: params[:list_name], todos: []}
   redirect "/lists"

39886539ecaf01568a9630227f94279a9785e84b
mskalisz <mskalisz3@gmail.com>
2022-05-22 19:14:43 -0400

    Initial commit

---
+++ todo.rb
@@ -0,0 +1,27 @@
+require "sinatra"
+require "sinatra/reloader"
+require "tilt/erubis"
+
+configure do
+  enable :sessions
+  set :session_secret, 'secret_word'
+end
+
+before do
+  session[:lists] ||= []
+end
+
+get "/" do
+  redirect "/lists"
+end
+
+get "/lists" do
+  @lists = session[:lists]
+
+  erb :lists, layout: :layout
+end
+
+get "/lists/new" do
+  session[:lists] << { name: "New List", todos: [] }
+  redirect "/lists"
+end

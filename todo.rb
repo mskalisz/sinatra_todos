bb6c0c7901f2d0a3b477d8b58f3ad298720dc107
mskalisz <mskalisz3@gmail.com>
2022-05-23 19:45:45 -0400

    Attempted adding javascript; added bugs

--- todo.rb
+++ todo.rb
@@ -30,8 +30,8 @@ helpers do
   def sort_lists(lists, &block)
     complete_lists, incomplete_lists = lists.partition { |list| list_complete?(list) }
 
-    incomplete_lists.each { |list| yield list, lists.index(list) }
-    complete_lists.each { |list| yield list, lists.index(list) }
+    incomplete_lists.each(&block)
+    complete_lists.each(&block)
   end
 
   def sort_todos(todos, &block)
@@ -48,6 +48,11 @@ helpers do
 
   incomplete_todos.each(&block)
   complete_todos.each(&block)
+  end
+
+  def next_todo_id(todos)
+    max = todos.map { |todo| todo[:id] }.max || 0
+    max + 1
   end
 end
 
@@ -87,12 +92,17 @@ def error_for_todo(name)
 end
 
 # Use a common method to load the list
-def load_list(index)
-  list = session[:lists][index] if index && session[:lists][index]
+def load_list(id)
+  list = session[:lists].find{ |list| list[:id] == id }
   return list if list
-  
+
   session[:error] = "The specified list was not found."
   redirect "/lists"
+end
+
+def next_element_id(elements)
+  max = elements.map { |element| element[:id] }.max || 0
+  max + 1
 end
 
 # Create a new list
@@ -104,16 +114,20 @@ post "/lists" do
     session[:error] = error
     erb :new_list, layout: :layout
   else
-    session[:lists] << {name: list_name, todos: []}
+    id = next_element_id(session[:lists])
+    session[:lists] << { id: id, name: list_name, todos: [] }
     session[:success] = "The list has been created."
     redirect "/lists"
   end
 end
 
-# Get a single list based on list ID
-get "/lists/:id" do 
-  @list_id = params[:id].to_i
-  @list = load_list(@list_id)
+# View a todo list
+get '/lists/:id' do
+  id = params[:id].to_i
+  list = load_list(id)
+  @list_name = list[:name]
+  @list_id = list[:id]
+  @todos = list[:todos]
   erb :list, layout: :layout
 end
 
@@ -141,18 +155,18 @@ post "/lists/:id" do
   end
 end
 
-# Delete the list from the session list array
+# Delete a todo list
 post "/lists/:id/destroy" do
   id = params[:id].to_i
-  session[:lists].delete_at(id)
-
+  session[:lists].reject! { |list| list[:id] == id }
+  session[:success] = "The list has been deleted."
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
     "/lists"
   else
-    session[:success] = "The list has been deleted."
     redirect "/lists"
   end
 end
+
 
 # Add a new todo to a list
 post "/lists/:list_id/todos" do
@@ -165,19 +179,21 @@ post "/lists/:list_id/todos" do
     session[:error] = error
     erb :list, layout: :layout
   else
-    @list[:todos] << {name: text, completed: false}
+    id = next_element_id(@list[:todos])
+    @list[:todos] << { id: id, name: text, completed: false }
+
     session[:success] = "The todo was added."
     redirect "/lists/#{@list_id}"
   end
 end
 
-# Delete the todo from a list
+# Delete a todo from a list
 post "/lists/:list_id/todos/:id/destroy" do
   @list_id = params[:list_id].to_i
   @list = load_list(@list_id)
 
   todo_id = params[:id].to_i
-  @list[:todos].delete_at(todo_id)
+  @list[:todos].reject! { |todo| todo[:id] == todo_id }
 
   if env["HTTP_X_REQUESTED_WITH"] == "XMLHttpRequest"
     status 204
@@ -187,14 +203,15 @@ post "/lists/:list_id/todos/:id/destroy" do
   end
 end
 
-# Update the status of the todo
+# Update the status of a todo
 post "/lists/:list_id/todos/:id" do
   @list_id = params[:list_id].to_i
   @list = load_list(@list_id)
 
   todo_id = params[:id].to_i
   is_completed = params[:completed] == "true"
-  @list[:todos][todo_id][:completed] = is_completed
+  todo = @list[:todos].find { |todo| todo[:id] == todo_id }
+  todo[:completed] = is_completed
 
   session[:success] = "The todo has been updated."
   redirect "/lists/#{@list_id}"

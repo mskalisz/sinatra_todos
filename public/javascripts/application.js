7eec367483aca9403689836dfaab42b3ded449a4
mskalisz <mskalisz3@gmail.com>
2022-05-23 17:43:28 -0400

    Add JS to add confirmation to deleting todos

--- public/javascripts/application.js
+++ public/javascripts/application.js
@@ -1,1 +1,14 @@
-console.log("this is a test.");
+$(function() {
+
+  $("form.delete").sumbit(function(event) {
+    event.preventDefualt();
+    event.stopPropagation();
+
+    var ok = confirm("Are you sure? This cannot be undone!")
+    if (ok) {
+      this.submit();
+    }
+
+  });
+
+});
